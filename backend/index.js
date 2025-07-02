2f634486f50d7cc1ccb48a7bfaafd6389949e958
Abrahan04 <jamasabanda@puce.edu.ec>
2025-07-02 17:57:51 -0500

    Crear end-point get productos

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,9 @@ import productosRouter from './routes/productos.js';
 
 const app = express();
 
-app.get('/', (req,res) => {
-    res.status(200).json('OK');
-    });
 
-    app.listen(3000, () => {
-        console.log('La aplicacion esta escuchando el puerto 3000');
-    });
+app.listen(3000, () => {
+    console.log('La aplicacion esta escuchando el puerto 3000');
+});
 
 export default app;
